5baa7694efc603e5ab8274865da1007a9bd22696
lmartinezd <oluanexo@gmail.com>
2018-04-02 18:10:03 -0300

    inicio do game, tela principal de game

---
+++ Assets/Scripts/scriptBackground.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptBackground : MonoBehaviour {
+ 
+	public float velocidade;
+	public float posicaoInicial, posicaoFinal;
+
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x <= posicaoFinal) {
+			transform.position = new Vector2 (posicaoInicial, transform.position.y);
+		}
+		transform.Translate (Vector2.left * velocidade * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/scriptPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptPlayer : MonoBehaviour {
+	Rigidbody2D rb;
+	public float impulso;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Jump")) {
+			rb.velocity = new Vector2 (0.0f, impulso);
+		}
+	}
+}
